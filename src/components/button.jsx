41bdeff0427c2876b609dc0c1c932e089863d91b
claudiajerome07 <claudiajerome07@gmail.com>
2025-01-20 16:55:45 +0530

    completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+
+import React from "react";
+
+function ButtonCard() {
+    return (
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
+            View Product
+        </button>
+    );
+}
+export default ButtonCard;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,23 @@
 // write product card here
+
+import React from "react";
+import ButtonCard from "./button";
+
+function ProductCard() {
+    const productImage ='https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ-lscUaxB5AXIXng2ePbNmjSW2IuxUleyQsYl7xat_VAv4EYVtoyqish94kiNbPjZ444lbVThOJKWXhNXOr0TFYwYENYhv2KmXTPJ9DBGh'
+    const productName='Coat'
+    const productPrice='$199'
+
+    return (
+        <div className="bg-white border border-gray-200 rounded-lg shadow-md p-6 max-w-sm text-center">
+            <img src={productImage} alt="Product" className="w-full h-48 object-cover rounded-t-lg mb-4" />
+            <div className="text-gray-700">
+                <h2 className="text-lg font-semibold mb-2">{productName}</h2>
+                <p className="text-md font-medium text-green-600 mb-4">{productPrice}</p>
+                <ButtonCard />
+            </div>
+        </div>
+    );
+
+}
+export default ProductCard;
